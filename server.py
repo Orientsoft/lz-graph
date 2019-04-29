47e4d31b5802b2cfebcb9e02ca9a699efa8cb551
Xie Di <xiedidan@yeah.net>
2019-04-29 18:21:53 +0800

    update

--- server.py
+++ server.py
@@ -1,14 +1,37 @@
 from multiprocessing.managers import BaseManager
-import Queue
+from multiprocessing import Queue
+import os
 
-task_queue = Queue.Queue()
-result_queue = Queue.Queue()
+import pandas as pd
+
+# consts
+root_path = '/home/voyager/project/lz-graph/data/'
+
+node_df = pd.read_csv(os.path.join(root_path, 'nodes.csv'))
+type_list = node_df['CUSTOMTYPE']
+
+link_df = pd.read_csv(os.path.join(root_path, 'stat_links.csv'))
+source_list = link_df['source']
+target_list = link_df['target']
+
+# init data
+init_dict = {
+    'node_count': len(type_list),
+    'source_list': source_list,
+    'target_list': target_list
+}
+
+task_queue = Queue()
+result_queue = Queue()
 
 class QueueManager(BaseManager): pass
 QueueManager.register('get_task_queue', callable=lambda:task_queue)
 QueueManager.register('get_result_queue', callable=lambda:result_queue)
+QueueManager.register('get_init_dict', callable=lambda:init_dict)
 
-manager = QueueManager(address=('', 50000), authkey='python_queue_server')
+manager = QueueManager(address=('0.0.0.0', 50000), authkey=b'asdfjkl;')
 
 server = manager.get_server()
 server.serve_forever()
+
+print('listening')
--- subgraph_worker.py
+++ subgraph_worker.py
@@ -1,42 +1,71 @@
+import os
+import multiprocessing
 from multiprocessing.managers import BaseManager
 
+from tqdm import tqdm
 import networkx as nx
 
-class QueueManager(BaseManager): pass
-QueueManager.register('get_task_queue', callable=lambda:task_queue)
-QueueManager.register('get_result_queue', callable=lambda:result_queue)
+cpu_count = os.cpu_count()
 
-manager = QueueManager(address=('', 50000), authkey='python_queue_server')
+class QueueManager(BaseManager): pass
+QueueManager.register('get_task_queue')
+QueueManager.register('get_result_queue')
+QueueManager.register('get_init_dict')
+
+manager = QueueManager(address=('192.168.0.46', 50000), authkey=b'asdfjkl;')
 
 manager.connect()
 task_queue = manager.get_task_queue()
-result_queue = manager.get_result_queue()
+# result_queue = manager.get_result_queue()
 
 # init
+init_data = manager.get_init_dict()
+node_count = init_data.get('node_count')
+source_list = init_data.get('source_list')
+target_list = init_data.get('target_list')
+
 G = nx.DiGraph()
-G.add_nodes_from(range(task['node_count']))
-G.add_edges_from([(task['source_list'][i], task['target_list'][i]) for i in range(len(task['source_list']))])
+G.add_nodes_from(range(node_count))
+for i in tqdm(range(len(source_list))):
+    G.add_edges_from([(source_list[i], target_list[i])])
+
+# G.add_edges_from([(source_list[i], target_list[i]) for i in range(2000)])
+
+print('networkX G - nodes: {}, edges: {}'.format(
+    G.number_of_nodes(),
+    G.number_of_edges()
+))
+
+task_buffer = []
+
+def worker_func(task):
+    node = task['node']
+    depth = task['bfs_depth']
+        
+    edges = list(nx.bfs_edges(G, node, depth_limit=depth))
+    nodes = [node] + [v for u, v in edges]
+
+    result = {
+        'edges': edges,
+        'nodes': nodes
+    }
+    
+    print('subG {} depth {} - nodes: {}, edges: {}'.format(
+        node,
+        depth,
+        len(nodes),
+        len(edges)
+    ))
+    
+    #result_queue = manager.get_result_queue()
+    #result_queue.put(result)
+
+def callback(result):
+    result_queue = manager.get_result_queue()
+    result_queue.put(result)
+
+pool = multiprocessing.Pool(cpu_count)
 
 while True:
     task = task_queue.get()
-    
-    if (task['type'] == 'init'):
-        
-        
-        result = {
-            'edge_count': G.number_of_edges(),
-            'node_count': G.number_of_nodes()
-        }
-        
-    elif (task['type'] == 'subgraph'):
-        edges = list(nx.bfs_edges(G, task['node'], depth_limit=task['bfs_depth']))
-        nodes = [node] + [v for u, v in edges]
-        
-        result = {
-            'edges': edges,
-            'nodes': nodes
-        }
-        
-        result_queue.put(result)
-
-    
+    pool.apply_async(worker_func, (task, ))
